84946a0bd7b135016e109bb626dbde54b7ba5672
Simon Bertolf <simon.bertolf@reamis.ch>
2019-12-04 09:48:38 +0100

    Save

--- clients/javascript.js
+++
@@ -1,8 +0,0 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
-
-$(document).ready(function(){
-    $("#sidebarbutton").click(function(){
-        $("#slide").slideToggle("slow");
-    });
-});
-
